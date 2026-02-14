1894f475dc0fbb94a8d2d01358e22c21845409d8
mailmichaelsr-afk <mail.michael.sr@gmail.com>
2026-02-14 15:32:47 -0600

    Create page.tsx

---
+++ app/runner/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Runner(){
+return(
+<div style={{padding:20}}>
+<h1>Runner Planning Dashboard</h1>
+<p>This becomes route planning, scheduling, availability later.</p>
+</div>
+);
+}
